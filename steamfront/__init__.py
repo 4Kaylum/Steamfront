05dd44962ff26c16173f05bfc4820888c9d5831f
Callum Bartlett <coolum444@hotmail.co.uk>
2017-01-25 23:58:23 +0000

    Another version bump
    I don't know what I'm doing any more please help.

--- steamfront/__init__.py
+++ steamfront/__init__.py
@@ -3,6 +3,12 @@ from .game import *
 from .exceptions import *
 from .user import *
 
+try:
+	from collections import namedtuple
+except ImportError:
+	VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+	version_info = VersionInfo(major=0, minor=0, micro=2, releaselevel='final', serial=0)
+
 __title__ = 'Steamfront'
 __author__ = 'Callum Bartlett'
 __license__ = 'MIT'
