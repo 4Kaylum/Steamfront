7095ac12639dbb6379294e266df760665bcab9e7
Callum Bartlett <coolum444@hotmail.co.uk>
2017-01-25 23:50:26 +0000

    Added version tag
    I don't know how this works someone help me.

--- steamfront/__init__.py
+++ steamfront/__init__.py
@@ -2,3 +2,9 @@ from .steamfront import *
 from .game import *
 from .exceptions import *
 from .user import *
+
+__title__ = 'Steamfront'
+__author__ = 'Callum Bartlett'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Callum Bartlett'
+__version__ = '0.0.2'
